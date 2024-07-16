85dd735855041048cf8af29e047fc7e173f25216
Bruna Melo <62797783+bqmelo@users.noreply.github.com>
2024-07-16 16:26:25 -0300

    Update AgricultorRepository.java

--- src/main/java/com/mapping/flood_impacts_mapping/repository/AgricultorRepository.java
+++ src/main/java/com/mapping/flood_impacts_mapping/repository/AgricultorRepository.java
@@ -1,9 +1,15 @@
 package com.mapping.flood_impacts_mapping.repository;
 
 import com.mapping.flood_impacts_mapping.entity.Agricultor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AgricultorRepository extends CrudRepository<Agricultor, Integer> {
+
+    @Query("SELECT a FROM Agricultor a WHERE (:razaoSocial IS NULL OR a.razaoSocial LIKE %:razaoSocial%)")
+    List<Agricultor> findAgricultoresByFilters(String razaoSocial);
 }
