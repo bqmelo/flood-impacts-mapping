4625520f3095c7c4bdb978384159cd7c9e1874e8
Bruna Melo <62797783+bqmelo@users.noreply.github.com>
2024-07-16 16:22:06 -0300

    Create CorsConfig.java

---
+++ src/main/java/com/mapping/flood_impacts_mapping/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.mapping.flood_impacts_mapping.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Authorization", "Content-Type")
+                .maxAge(3600);
+    }
+}
