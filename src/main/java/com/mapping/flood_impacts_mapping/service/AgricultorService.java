64d285626eda00fdaf5593ad03598c9da33cc5d3
Bruna Melo <62797783+bqmelo@users.noreply.github.com>
2024-07-16 16:26:43 -0300

    Update AgricultorService.java

--- src/main/java/com/mapping/flood_impacts_mapping/service/AgricultorService.java
+++ src/main/java/com/mapping/flood_impacts_mapping/service/AgricultorService.java
@@ -1,11 +1,12 @@
 package com.mapping.flood_impacts_mapping.service;
 
 import com.mapping.flood_impacts_mapping.entity.Agricultor;
+import io.swagger.models.auth.In;
 
 import java.util.List;
 
 public interface AgricultorService {
-    List<Agricultor> getAllAgricultores();
+    List<Agricultor> getAllAgricultores(String agricultorName);
     Agricultor getAgricultorById(int id);
     Agricultor createAgricultor(Agricultor agricultor);
     Agricultor updateAgricultor(Agricultor agricultor);
